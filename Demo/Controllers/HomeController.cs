e7f68c1d6c5c4d4a4401f1d952396b2a31d9fcb0
Pandora Vanity <pandora.va7@gmail.com>
2025-07-07 22:12:17 +0800

    添加项目文件。

---
+++ Demo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Demo1(string name, int age)
+    {
+        ViewBag.Name = name;
+        ViewBag.Age = age;
+        return View();
+    }
+
+    public IActionResult Demo2(string name, int age)
+    {
+        var m = new
+        {
+            Name = name,
+            Age = age,
+        };
+        return View(m);
+    }
+
+    [Route("ABC")]
+    public IActionResult Demo3()
+    {
+        return View("123");
+    }
+}
