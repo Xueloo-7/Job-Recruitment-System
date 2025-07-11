dc32e8c4ace5ded12a969c3b5c0a1418204752af
Pandora Vanity <pandora.va7@gmail.com>
2025-07-11 11:11:19 +0800

    db update

---
+++ Demo/Models/DB.cs
@@ -0,0 +1,33 @@
+﻿namespace Demo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+#nullable disable warnings
+
+public class DB : DbContext
+{
+    public DB(DbContextOptions options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+}
+public class User
+{
+    public int Id { get; set; }
+    [MaxLength(20)]
+    public string Username { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string PasswordHash { get; set; }
+    [EmailAddress]
+    [MaxLength(100)]
+    public string Email { get; set; }
+    [Phone]
+    [MaxLength(20)]
+    public string PhoneNumber { get; set; }
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,12 @@
+using Demo.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSqlServer<DB>($@"
+    Data Source=(LocalDB)\MSSQLLocalDB;
+    AttachDbFilename={builder.Environment.ContentRootPath}\DB.mdf;
+");
 
 var app = builder.Build();
 app.UseHttpsRedirection();
