b5150ceda3d9bc50e21133a7aa2a8f0e0a10750e
Rayan-Loh <kaixiangloh269@gmail.com>
2025-07-19 01:36:12 +0800

    cshtml,css,js, controller file created...

---
+++ Demo/Controllers/JobController.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+//---Put in first because no database in Models---
+
+
+//public IActionResult SearchAjax(string? keyword, string? location)
+//{
+//    keyword = keyword?.Trim().ToLower() ?? "";
+//    location = location?.Trim().ToLower() ?? "";
+
+//    var result = db.Jobs
+//        .Where(j =>
+//            (string.IsNullOrEmpty(keyword) || j.Title.ToLower().Contains(keyword)) &&
+//            (string.IsNullOrEmpty(location) || j.Location.ToLower().Contains(location)))
+//        .ToList();
+
+//    return PartialView("_JobListPartial", result);
+//}
