da6f459397dcfd954eb2089f9e227520b6f346a5
Pandora <pandora.va7@gmail.com>
2025-08-03 20:57:21 +0800

    perf: 将Promotion Card的生成改为Ajax生成 feat: 新增了Extension.cs

--- Demo/wwwroot/js/app.js
+++ Demo/wwwroot/js/app.js
@@ -1,9 +1,9 @@
 ﻿//// Initiate GET request (AJAX-supported)
-//$(document).on('click', '[data-get]', e => {
-//    e.preventDefault();
-//    const url = e.target.dataset.get;
-//    location = url || location;
-//});
+$(document).on('click', '[data-get]', e => {
+    e.preventDefault();
+    const url = e.target.dataset.get;
+    location = url || location;
+});
 
 
 //---Side Menu---
