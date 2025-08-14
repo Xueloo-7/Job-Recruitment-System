8fe12fd85135d6bf38cee93a54ec3ecf223f42f7
Rayan-Loh <kaixiangloh269@gmail.com>
2025-08-14 12:34:18 +0800

    erererererer..................................................................................................................................................................................

--- Demo/Controllers/ProfileController.cs
+++ Demo/Controllers/ProfileController.cs
@@ -1,17 +1,63 @@
 ﻿using Demo.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Demo.Controllers;
 public class ProfileController : Controller
 {
-    public IActionResult Index()
+    private readonly DB db;
+
+    User? currentUser = null;
+
+    // 依赖注入
+    public ProfileController(DB context)
     {
-        return View();
+        db = context;
     }
 
+    // 模拟数据
+    private static User demoUser = new User
+    {
+        FirstName = "Xueloo",
+        LastName = "Chan",
+        Location = "Kuala Lumpur",
+        PhoneNumber = "012-3456789"
+    };
+    public IActionResult Index(string? userId = "")
+    {
+        var user = db.Users.Find(userId);
+        if(user != null)
+        {
+            currentUser = user;
+        }
+        else
+        {
+            // 如果没有传入 userId，则使用模拟数据
+            currentUser = demoUser;
+        }
+
+        return View(user);
+    }
+
+    public IActionResult EditPartial()
+    {
+        return PartialView("_Edit", currentUser);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(User updatedUser)
+    {
+        // 更新逻辑
+        currentUser = updatedUser;
+
+        // 返回更新后的 Profile 视图
+        return PartialView("_Profile", currentUser);
+    }
+     
     public IActionResult Profile()
     {
-        return PartialView("_Profile");
+        Debug.WriteLine(currentUser);
+        return PartialView("_Profile", currentUser);
     }
 
     public IActionResult Summary()
