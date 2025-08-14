ac7ab3f8d67c80f23b525576b866f045400bf1d3
Pandora <pandora.va7@gmail.com>
2025-08-14 17:17:56 +0800

    feat: report basic ui

---
+++ Demo/Controllers/ReportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ReportController : Controller
+{
+    private readonly DB _db;
+
+    public ReportController(DB context)
+    {
+        _db = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
