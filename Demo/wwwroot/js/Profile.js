798f38187c461f0cff2845f909b15a850ba7c195
KAI XIANG RAYAN LOH <kaixiangloh269@gmail.com>
2025-08-30 10:28:05 +0800

    fixed profile bug

--- Demo/wwwroot/js/Profile.js
+++ Demo/wwwroot/js/Profile.js
@@ -1,32 +1,13 @@
 ﻿$(function () {
+    // Tab 按钮点击事件，只处理 UI 样式
     $('.tab-button').click(function () {
-        console.log("sss");
-        // 更新按钮状态
+        // 移除所有按钮的 active
         $('.tab-button').removeClass('active');
+        // 当前按钮加 active
         $(this).addClass('active');
-
-        // 获取 tab 名称（从 data-tab 属性）
-        var tab = $(this).data('tab');
-
-        // 显示 Loading
-        $('#tab-content').html('<p>Loading...</p>');
-
-        // AJAX 加载对应 Partial View
-        $.ajax({
-            url: '/Profile/' + tab,
-            method: 'GET',
-            success: function (data) {
-                $('#tab-content').html(data);
-            },
-            error: function () {
-                $('#tab-content').html('<p style="color:red;">Failed to load content.</p>');
-            }
-        });
     });
-
-    // 默认加载 Profile 内容（触发默认激活按钮点击）
-    $('.tab-button.active').trigger('click');
 });
+
 
 $(document).on('click', '#edit-btn', function () {
     var userId = $(this).data('user-id');
